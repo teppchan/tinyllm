3ac78d02e973235f5cf9a47657708b4fc905558f
Teppchan <teppchan+github@github.com>
2024-07-26 16:53:48 +0900

    update params of `gpt`

--- src/tinyllm2/inference.py
+++ src/tinyllm2/inference.py
@@ -19,6 +19,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 @click.option("--num_heads", type=int, default=6)
 @click.option("--depth", type=int, default=6)
 @click.option("--sentence_size", type=int, default=1024)
+@click.option("--drop_out_rate", type=float, default=0.1)
+@click.option("--layer_eps", type=float, default=1.0e-5)
 @click.option("--text", type=str, default="これはなに？")
 def main(
     tokenizer_file: str,
@@ -27,6 +29,8 @@ def main(
     num_heads: int,
     depth: int,
     sentence_size: int,
+    drop_out_rate: float,
+    layer_eps: float,
     text: str,
 ):
     checkpoint = torch.load(model_file, map_location=device)
@@ -39,17 +43,19 @@ def main(
     print(f"vocab_size = {vocab_size}")
 
     gpt = GPT(
-        vocab_size,
-        embedding_size,
-        embedding_size * 4,
-        num_heads,
-        0.0,
+        vocab_size=vocab_size,
+        embedding_dim=embedding_size,
+        ffn_dim=embedding_size * 4,
+        num_heads=num_heads,
+        drop_out_rate=drop_out_rate,
+        layer_eps=layer_eps,
         batch_first=True,
         T=sentence_size,
         N=depth,
     ).to(device)
 
     gpt.load_state_dict(checkpoint["model"])
+    print(f"params = {gpt.calculate_params():,}")
 
     print(
         gpt.generate_sentence(
